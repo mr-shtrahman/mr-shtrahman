7ad86ad7b470517c63f285e234af09e6366fb3d1
Jack Z <jackzaf96@hotmail.com>
2021-05-19 20:55:55 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mr_shtrahman.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
