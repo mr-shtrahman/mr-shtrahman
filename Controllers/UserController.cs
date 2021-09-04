545f1517a3625294ba7ffd781974b96900dc58f3
Orel Zluf <orelz16@gmail.com>
2021-09-04 23:12:20 +0300

    Added login option from google api. Get the user profile details and send it with ajax request to the backend.
    Need to fix the logout function.
    Need to save the user details in cookie.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mr_shtrahman.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjectDBContext = mr_shtrahman.Data.Context;
+
+namespace mr_shtrahman.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: User/Search
+        public ActionResult Search(string email = null)
+        {
+            return RedirectToAction("Index", "Error", new { message = "You have to login first.." });
+        }
+
+        // POST: User/Login
+        [HttpPost]
+        public ActionResult Login([FromBody] User user)
+        {
+            // TODO: If the user exists in DB - check if the user is an admin
+            // Else - this is the first login, save the user in DB
+
+            return Json(user);
+        }
+
+        // GET: User/Logoff
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
