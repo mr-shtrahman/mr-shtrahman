1f40cf7507985a58e155cfd885729a534c21e5ed
mikim <mikimeerson44@gmail.com>
2021-09-06 18:12:06 +0300

    add groupby for trips

--- Controllers/TripsController.cs
+++ Controllers/TripsController.cs
@@ -80,7 +80,9 @@ namespace mr_shtrahman.Controllers
         // GET: Trips/Create
         public IActionResult Create()
         {
-            ViewData["Products"] = new SelectList(_context.Product, nameof(Product.Id), nameof(Product.Name));
+            var ProductByCategory = _context.Product.AsEnumerable().GroupBy(p => p.Category).ToDictionary(g => g.Key, g => g.ToList()); ;
+
+            ViewData["Products"] = ProductByCategory;
             ViewData["VisitorsAttendance"] = new SelectList(_context.VisitorsAttendance, nameof(VisitorsAttendance.Id), nameof(VisitorsAttendance.Date));
             ViewData["Images"] = new SelectList(_context.Img.Where(i => i.ShopId == null && i.TripId == null && i.ProductId == null), nameof(Img.Id), nameof(Img.Src));
             return View();
@@ -122,8 +124,10 @@ namespace mr_shtrahman.Controllers
                 return NotFound();
             }
 
+            var ProductByCategory = _context.Product.AsEnumerable().GroupBy(p => p.Category).ToDictionary(g => g.Key, g => g.ToList()); ;
+
+            ViewData["Products"] = ProductByCategory;
             ViewData["Image"] = _context.Img.Where(i => i.ShopId == null && i.TripId == id && i.ProductId == null).FirstOrDefault();
-            ViewData["Products"] = new SelectList(_context.Product, nameof(Product.Id), nameof(Product.Name));
             ViewData["VisitorsAttendance"] = new SelectList(_context.VisitorsAttendance, nameof(VisitorsAttendance.Id), nameof(VisitorsAttendance.Date));
             ViewData["Images"] = new SelectList(_context.Img.Where(i => i.ShopId == null && (i.TripId == id || i.TripId == null) && i.ProductId == null), nameof(Img.Id), nameof(Img.Src));
             return View(trip);
