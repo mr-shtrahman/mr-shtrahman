207f8bd0e6c76f9f1d78a82227006219a3769ac3
Orel Zluf <orelz16@gmail.com>
2021-09-06 16:31:31 +0300

    changed back ContextModelVersion

--- Migrations/ContextModelSnapshot.cs
+++ Migrations/ContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace mr_shtrahman.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.8")
+                .HasAnnotation("ProductVersion", "5.0.6")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("ProductShop", b =>
